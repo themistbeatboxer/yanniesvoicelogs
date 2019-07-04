29cc4fa270917a0c5ae891353346d69a596ed98e
tasnimahmed <tasnimahmed102001@gmail.com>
2019-07-04 01:04:40 -0400

    audio1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+isPlaying = false;
+function login()
+{
+	arrFields = document.getElementsByClassName("inputField");
+	if(arrFields[0].value == "beeboobeeboo" && arrFields[1].value == "tastaslovesyou")
+	{
+		window.location.href = "webPages/letter.html";
+	}
+	else
+	{
+		alert("INCORRECT USERNAME AND/OR PASSWORD");
+	}
+}
+function playAudio(filePath)
+{
+	if(isPlaying)
+		return;
+	var audio = new Audio("audio/" + filePath + ".aac");
+	audio.play();
+	isPlaying = true;
+	audio.onended = function() {isPlaying = false;}
+}
